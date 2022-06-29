bd1a31bdb277c782b6a769bad7f62348ec0494b2
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2022-06-30 10:18:13 +1200

    adding tests

---
+++ test.py
@@ -0,0 +1,12 @@
+def test_valid(cldf_dataset, cldf_logger):
+    assert cldf_dataset.validate(log=cldf_logger)
+
+
+# should be 210 items
+def test_parameters(cldf_dataset):
+    assert len(list(cldf_dataset["ParameterTable"])) == 210
+
+
+# test we have some languages
+def test_languages(cldf_dataset):
+    assert len(list(cldf_dataset["LanguageTable"])) == 205
